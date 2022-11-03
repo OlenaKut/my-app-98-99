ffcc84769a9940b87840c837e67e3c0b61fa960a
OlenaKut <cundrochka@gmail.com>
2022-11-03 23:53:29 +0100

    Added timer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import "./App.css";
 import User from "./User";
 import { Ellipsis } from "react-css-spinners";
 import { useTimer } from "use-timer";
-import Timer from "react-timer";
 
 function App() {
   const [data, setData] = useState("");
   const [load, setLoad] = useState(false);
   const [error, setError] = useState("");
 
-  const OPTIONS = { prefix: "seconds elapsed!", delay: 200 };
-
-  const { time, start, pause, reset, status } = useTimer();
+  const { time, start, pause, reset, status } = useTimer({ autostart: true });
 
   useEffect(() => {
     setLoad(true);
@@ -22,7 +19,7 @@ function App() {
         .then((response) => setData(response))
         .then(() => setLoad(false))
         .catch(setError);
-    }, 3000);
+    }, 2000);
   }, []);
 
   if (error) {
@@ -41,29 +38,25 @@ function App() {
       <div className="App container shadow-lg rounded-2 p-2">
         <User name={data.login} avatar={data.avatar_url} />
         <div className="d-flex justify-content-center line shadow py-4">
-          <h6 className="col-3">You are lookig for me </h6>
-          <div className="col-6 timer">
-            <Timer options={OPTIONS} />
+          <h6 className="col-4">You are lookig for me </h6>
+          <div className="col-5 timer">
+            <h5 className="text-danger">{time}</h5>
+            {status === "RUNNING"}
+            <div className="buttons">
+              <button onClick={start} className="btn btn-danger m-1">
+                Start
+              </button>
+              <button onClick={pause} className="btn btn-danger m-1">
+                Pause
+              </button>
+              <button onClick={reset} className="btn btn-danger m-1">
+                Reset
+              </button>
+            </div>
           </div>
 
           <h6 className="col-3">seconds</h6>
         </div>
-
-        <>
-          <div className="App container shadow rounded-2 p-2">
-            <p>Elapsed time: {time}</p>
-            {status === "RUNNING" && <p>Running...</p>}
-            <button onClick={start} className="btn btn-danger m-1">
-              Start
-            </button>
-            <button onClick={pause} className="btn btn-danger m-1">
-              Pause
-            </button>
-            <button onClick={reset} className="btn btn-danger m-1">
-              Reset
-            </button>
-          </div>
-        </>
       </div>
     );
   } else {
